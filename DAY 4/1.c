cf555a10c97cce7a9f2a210e93df92b25738e6e2
Shaurya206 <shauryasinghal706@gmail.com>
2025-08-31 03:27:58 +0530

    Add files via upload

---
+++ DAY 4/1.c
@@ -0,0 +1,21 @@
+//Write a program to swap two numbers without using a third variable.
+
+
+#include<stdio.h>
+int main(){
+
+    int a,b;
+
+    
+    printf("enter first number: ");           // input first number
+    scanf("%d",&a);
+
+    printf("enter second number: ");         // input second number
+    scanf("%d",&b);
+
+
+    printf("%d %d",b,a);
+
+    return 0;
+
+}                                           // end
